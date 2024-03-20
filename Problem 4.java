2c9a190c7973ddd94527bdbe3e733aac6a933532
sahithi-sahi07 <105721088+sahithi-sahi07@users.noreply.github.com>
2024-03-20 11:31:33 +0530

    Update Problem 4.java

--- Problem 4.java
+++ Problem 4.java
@@ -1,15 +1,18 @@
- class MyStack {
-    Queue<Integer> q = new LinkedList<Integer>();
-    
 
+class MyStack {
+    Queue<Integer> q = new LinkedList<Integer>();
+
+    // Push element x onto stack.
     public void push(int x) {
         q.add(x);
-        
-        int n = q.size();
-        while (n-- > 1)
+        int size = q.size();
+        while (size > 1) {
             q.add(q.poll());
+            size--;
+        }
     }
 
+    // Removes the element on top of the stack.
     public void pop() {
         q.poll();
     }
@@ -19,9 +22,8 @@
         return q.peek();
     }
 
-
+    // Return whether the stack is empty.
     public boolean empty() {
         return q.isEmpty();
     }
-
-  }
+}
